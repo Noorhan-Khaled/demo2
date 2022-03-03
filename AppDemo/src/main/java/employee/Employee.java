210281aee318fd8aa06da9a462c0ef031032f907
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-03-03 14:46:50 +0200

    Add Employee

---
+++ AppDemo/src/main/java/employee/Employee.java
@@ -0,0 +1,32 @@
+package employee;
+
+public class Employee {
+
+    private Integer employeeId ;
+    private String name  ;
+    private Integer salary ;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
