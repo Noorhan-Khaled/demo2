e354e1b1097329c021614c203266ad1ee96eb6fb
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-03-03 15:52:16 +0200

    Add testEmployee add employees modified

--- AppDemo/src/main/java/employee/TestEmployee.java
+++ AppDemo/src/main/java/employee/TestEmployee.java
@@ -3,8 +3,8 @@ package employee;
 public class TestEmployee {
 
     public static void main(String[] args){
-        int n=10;
-        Employee[] employee=new Employee[n];
+      //  int n=10;
+        Employee[] employee=new Employee[10];
         for (int i = 1; i <= employee.length; i++) {
             employee[i]= new Employee(i,"name_"+i,i+100);
             System.out.println(employee[i].getEmployeeId()+" "+employee[i].getName()+" "+employee[i].getSalary());
